f99fcf292853a1ab22072507b0f69b5d6c838acd
Sam Zagrobelny <strangesast@gmail.com>
2015-01-27 11:36:49 -0500

    added serialport list function, updated config, updated index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.js');
+var serialPort = require('serialport');
 
-/* GET home page. */
+function listPorts() {
+	return new Promise(function(resolve, reject) {
+	  serialPort.list(function(err, ports) {
+	  	if(err !== null) reject(err);
+	  	else resolve(ports);
+	  });
+	});
+}
+
+var sentAlong = {
+	'dbaddress' : config.databaseAddress,
+	'title'     : config.title
+  };
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	listPorts().then(function(ports) {
+		sentAlong.ports = ports;
+    res.render('index', sentAlong);
+	},
+	function(err) {
+		res.send('failed to grab ports');
+	});
 });
 
 module.exports = router;
